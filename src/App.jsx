cbe40e37153fc83d23a1b719c761de4e61d6c684
Hemish Patel <Hemishpatel2005@gmail.com>
2026-04-14 23:44:45 +1200

    Created structure of the whole app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import Projects from "@/pages/Projects";
+import Contact from "@/pages/Contact";
+import ProjectDetail from "@/pages/ProjectDetail";
+
 function App() {
   return (
-    <div className="min-h-screen overflow-x-hidden">
+    <BrowserRouter>
       <Navbar />
-      <main></main>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<ProjectDetail />} />
+        <Route path="/projects/:id" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
